b01df9d38ee20248dd180b356fe61900f60f2446
Gabriel Dewes <gabrieldewes@outlook.com>
2016-04-09 05:03:23 -0300

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -72,13 +72,35 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $active_group = 'default';
 $query_builder = TRUE;
-
+/*
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'slave_db_teste',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+*/
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'slavedb_teste.mysql.dbaas.com.br',
+	'username' => 'slavedb_teste',
+	'password' => 'Peixeira',
+	'database' => 'slavedb_teste',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
